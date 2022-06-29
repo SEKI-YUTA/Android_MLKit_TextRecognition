28383f751f47252ad8983d1908876311af845d76
SEKI-YUTA <zard.since1991@gmail.com>
2022-06-29 21:50:10 +0900

    finish implement basic text recognition

--- app/src/main/java/com/example/textrecognition/MainActivity.java
+++ app/src/main/java/com/example/textrecognition/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.textrecognition;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -15,7 +18,11 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
 import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.TextRecognizer;
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
@@ -26,9 +33,10 @@ public class MainActivity extends AppCompatActivity {
 
 //    https://developers.google.com/ml-kit/vision/text-recognition/android#java
     private final int PICK_IMAGE = 1;
+    private boolean isImgPicked = false;
     TextRecognizer recognizer;
     ImageView img_pickedImg;
-    Button btn_imgPick;
+    Button btn_imgPick, btn_imgProcess;
     InputImage inputImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +45,32 @@ public class MainActivity extends AppCompatActivity {
 
         img_pickedImg = findViewById(R.id.img_pickedImg);
         btn_imgPick = findViewById(R.id.btn_picImage);
+        btn_imgProcess = findViewById(R.id.btn_imgProcess);
+
+        btn_imgProcess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isImgPicked) return;
+                Task<Text> result =
+                        recognizer.process(inputImage)
+                                .addOnSuccessListener(new OnSuccessListener<Text>() {
+                                    @Override
+                                    public void onSuccess(Text text) {
+                                        Toast.makeText(MainActivity.this, "Text recognition successed", Toast.LENGTH_SHORT).show();
+                                        Log.d("Process result", text.getText());
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(MainActivity.this, "Text recognition failed", Toast.LENGTH_SHORT).show();
+
+                                    }
+                                });
+
+            }
+        });
+
 
         btn_imgPick.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,6 +97,7 @@ public class MainActivity extends AppCompatActivity {
 
             try {
                 inputImage = InputImage.fromFilePath(this, selectedImage);
+                isImgPicked = true;
                 Toast.makeText(this, "InputImage set successed", Toast.LENGTH_SHORT).show();
             } catch (IOException e) {
                 e.printStackTrace();
